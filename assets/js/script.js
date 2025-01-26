87592c17533f5bc9e4897bb6d0a04f0284b0eb06
vishvavichu <114342485+vishvavichu@users.noreply.github.com>
2025-01-26 15:50:54 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend developer",  "web designer",  "web developer","Junior Python Developer"],
+    strings: ["frontend developer",  "Gen AI Engineer",  "web developer","Python Developer"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
